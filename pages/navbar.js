ea9eb23965ff9ec21b40066961cf76645e41dfda
Renzo Damian <renzo.damian.go@gmail.com>
2022-01-05 21:16:21 -0500

    add navbar content and logo

--- pages/navbar.js
+++ pages/navbar.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react'
+import styles from '../styles/Navbar.module.scss'
 
 export const Navbar = ()=>  {
 
     return (
         <nav>
-            <ul>
-                <li>Quienes somos</li>
-                <li>Productos</li>
-                <li>Servicios</li>
-                <li>Contactanos</li>
-            </ul>
+            <div className={styles.container}>
+                <img src="imgs/rtlogo.png" alt="Biotechnology Logo" />
+                <ul id={styles.nav}>
+                    <li><a href="">Quienes somos</a> </li>
+                    <li><a href="">Productos</a> </li>
+                    <li><a href="">Servicios</a> </li>
+                    <li><a href="">Contáctenos</a> </li>
+                </ul>
+            </div>
+
         </nav>
         )
 
